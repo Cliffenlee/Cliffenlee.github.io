4b99c50a72e0c7e2f93a7928643b058835a5c99a
Cliffenlee <cliffen.lee.2018@sis.smu.edu.sg>
2022-12-17 23:47:29 +0800

    add app shell

---
+++ next/portfolio/pages/app.tsx
@@ -0,0 +1,9 @@
+import Menu from "./menu";
+
+export default function App(): JSX.Element {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
---
+++ next/portfolio/pages/menu.tsx
@@ -0,0 +1,9 @@
+export default function Menu() {
+  return (
+    <ul>
+      <li>About Me</li>
+      <li>Experiences</li>
+      <li>Projects</li>
+    </ul>
+  );
+}
