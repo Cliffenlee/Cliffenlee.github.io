8863c1bb422fce91d66385edc7e28dce70b37fd2
Cliffenlee <cliffen.lee.2018@sis.smu.edu.sg>
2023-01-13 21:29:38 +0800

    Intro page

--- next/portfolio/pages/components/Intro/intro.tsx
+++ next/portfolio/pages/components/Intro/intro.tsx
@@ -3,18 +3,17 @@ import styles from "./intro.module.css";
 export default function Intro({}: {}): JSX.Element {
   return (
     <div className={styles.root}>
-      {/* <div className={styles.background}>
-        JL
-        </div> */}
-      <div className={styles.intro}>
-        <div>
-          <span className={styles.greeting}>Hello!</span>
+      <span className={styles.bigFont}>JEFF LEE JUN YAN</span>
+      <span className={`${styles.hollowFont} ${styles.middle}`}>
+        DIGITAL PORTFOLIO
+      </span>
+      <span className={styles.hollowFont}>SHOWCASE</span>
+      <div className={styles.caption}>
+        <div className={styles.smallFont}>
+          A FIRST YEAR NATIONAL UNIVERSITY OF SINGAPORE ARCHITECTURE STUDENT’S
         </div>
-        <div className={styles.name}>My name is Jeff.</div>
-        <div className={styles.description}>
-          I am a first year Architecture student from the National University of
-          Singapore. Always hungry for knowledge, I am on the hunt for great
-          opportunities to hone my expertise in design.
+        <div className={styles.smallFont}>
+          PROJECTS, PERSONAL WORK AND SKILL SHOWCASE
         </div>
       </div>
     </div>
